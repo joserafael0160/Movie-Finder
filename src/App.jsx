fb1107e313287d902d3c06258cabefb180d42c25
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-07-26 18:44:19 -0400

    Changing buscar-> search

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="page">
       <header>
-        <h1>Buscador de peliculas</h1>
+        <h1>Movie Finder</h1>
         <form className="form">
           <input placeholder="Avengers, Star Wars, The Matrix" />
-          <button>Buscar</button>
+          <button>Search</button>
         </form>
       </header>
 
