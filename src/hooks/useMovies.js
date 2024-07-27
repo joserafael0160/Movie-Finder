9676445450cb4b495879bc7ed498b22fbe24d2a2
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-07-27 19:35:19 -0400

    avoid the same search twice

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,14 +1,16 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { searchMovies } from "../services/movies"
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
-
+  const previousSearch = useRef(search)
   const getMovies = async () => {
+    if(search === previousSearch.current) return 
     try {
       setLoading(true)
       setError(null)
+      previousSearch.current = search
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
     } catch(e) {
