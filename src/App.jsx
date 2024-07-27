3591b971d8fe6bc1babc3ec0b657994a7617a030
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-07-27 18:49:15 -0400

    Grid responsive and search validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,68 @@
 import "./App.css"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useMovies } from "./hooks/useMovies"
 import { Movies } from "./components/Movies"
 
-function App() {
-  const {movies} = useMovies()
+function useSearch() {
+  const [search, updateSearch] = useState("")
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
 
+  useEffect(() => {
+    
+
+    if(search === "") {
+      if(isFirstInput.current) {
+        isFirstInput.current = search === ""
+        return
+      }
+      else {
+        setError("you can't search for an empty movie")
+        return 
+
+      }
+    }
+
+    if(search.match(/^\d+$/)) {
+      setError("you can't search for a movie with a number")
+      return
+    }
+
+    if(search.length < 3) {
+      setError("you can't search for a movie with less than 3 characters")
+      return
+    }
+    
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
+function App() { 
+  const { movies } = useMovies()
+  const { search, updateSearch, error } = useSearch()
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log({ search })
+  }
+
+  function handleChange(event) {
+    updateSearch(event.target.value)
+  }
   return (
     <div className="page">
       <header>
         <h1>Movie Finder</h1>
-        <form className="form">
-          <input placeholder="Avengers, Star Wars, The Matrix" />
+        <form className="form" onSubmit={handleSubmit}>
+          <input 
+            style={{
+              border: "1px solid transparent",
+              borderColor: error ? "red" : "transparent",
+            }} onChange={handleChange} value={search} name="query" placeholder="Avengers, Star Wars, The Matrix" />
           <button>Search</button>
         </form>
+        {error && <p style={{color: "red"}}>{error}</p>}
       </header>
 
       <main>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,10 +3,10 @@ import PropTypes from "prop-types"
 function ListOfMovies ({ movies }) {
   return (
     
-    <ul>
+    <ul className="movies">
       {
         movies.map(movie => (
-          <li key={movie.id}>
+          <li className="movie" key={movie.id}>
             <h3>{movie.title}</h3>
             <p>{movie.year}</p>
             <img src={movie.poster} alt={movie.title} />
